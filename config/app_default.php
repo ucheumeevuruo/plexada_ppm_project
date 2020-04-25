f1597f61cbc744802e6d4cbda3b559518ef44282
ucheumeevuruo <ucheumeevuruo@yahoo.com>
2020-04-25 02:06:49 +0100

    Update app_default.php

--- config/app_default.php
+++ config/app_default.php
@@ -1,17 +1,20 @@
 <?php
-'debug_kit' => [
-    'className' => 'Cake\Database\Connection',
-    'driver' => 'Cake\Database\Driver\Mysql',
-    'persistent' => false,
-    'host' => 'ec2-46-137-156-205.eu-west-1.compute.amazonaws.com',
-    'port' => '5432',
-    'username' => 'eobfgjvacoqygt',    // Your DB username here
-    'password' => '19088dae2aebd163545396551ee613c20af9d3e988963661a4b764d0a6d163fa',    // Your DB password here
-    'database' => 'd1eqp6e36t6rhs',
-    'encoding' => 'utf8',
-    'timezone' => 'UTC',
-    'cacheMetadata' => true,
-    'quoteIdentifiers' => false,
-    //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
+return
+[
+	'debug_kit' => [
+		'className' => 'Cake\Database\Connection',
+		'driver' => 'Cake\Database\Driver\Mysql',
+		'persistent' => false,
+		'host' => 'ec2-46-137-156-205.eu-west-1.compute.amazonaws.com',
+		'port' => '5432',
+		'username' => 'eobfgjvacoqygt',    // Your DB username here
+		'password' => '19088dae2aebd163545396551ee613c20af9d3e988963661a4b764d0a6d163fa',    // Your DB password here
+		'database' => 'd1eqp6e36t6rhs',
+		'encoding' => 'utf8',
+		'timezone' => 'UTC',
+		'cacheMetadata' => true,
+		'quoteIdentifiers' => false,
+		//'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
+	]
 ]
 ?>
