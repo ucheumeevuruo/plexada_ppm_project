35544c1624de8823f130c931c56ac60cc9e73723
kingsley okpara <kingsconsult001@gmail.com>
2020-06-25 23:48:09 +0100

    show notifications of tasks, activities, and indicators that are overdue

--- src/View/Helper/MessageHelper.php
+++ src/View/Helper/MessageHelper.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\View\Helper;
 
 use Cake\ORM\TableRegistry;
@@ -22,7 +23,9 @@ class MessageHelper extends Helper
         parent::__construct($View, $config);
 
         $this->Messages = TableRegistry::getTableLocator()->get('Messages');
-
+        $this->Tasks = TableRegistry::getTableLocator()->get('Tasks');
+        $this->Activities = TableRegistry::getTableLocator()->get('Activities');
+        $this->Milestones = TableRegistry::getTableLocator()->get('Milestones');
     }
 
     public function getMessageCount()
@@ -45,4 +48,113 @@ class MessageHelper extends Helper
 
         return $messages;
     }
+
+    public function getTasks()
+    {
+        $tasks = $this->Tasks->find('all')->where(['status_id' => 1]);
+        $today = date_create(date('m/d/Y'));
+
+        $count = 0;
+        $convertDate = array();
+        foreach ($tasks as $task) {
+            $dateDiff = date_diff($today, date_create($task->Start_date));
+            $converted = $dateDiff->format('%R%a day(s)');
+            if ((int) $converted < 0) {
+
+                array_push($convertDate, $task);
+                // array_push($convertDate, (int) $converted, $task->Task_name);
+            }
+
+            $count++;
+        }
+        return $convertDate;
+    }
+    public function tasksCount()
+    {
+        $tasks = $this->Tasks->find('all')->where(['status_id' => 1]);
+        $today = date_create(date('m/d/Y'));
+
+        $count = 0;
+        foreach ($tasks as $task) {
+            $dateDiff = date_diff($today, date_create($task->Start_date));
+            $converted = $dateDiff->format('%R%a day(s)');
+            if ((int) $converted < 0) {
+                $count++;
+            }
+        }
+        return $count;
+    }
+   
+
+    public function getActivities()
+    {
+        $activities = $this->Activities->find('all')->where(['status_id' => 1]);
+        $today = date_create(date('m/d/Y'));
+
+        $count = 0;
+        $convertDate = array();
+        foreach ($activities as $activity) {
+            $dateDiff = date_diff($today, date_create($activity->start_date));
+            $converted = $dateDiff->format('%R%a day(s)');
+            if ((int) $converted < 0) {
+
+                array_push($convertDate, $activity);
+            }
+
+            $count++;
+        }
+        return $convertDate;
+    }
+    public function activitiesCount()
+    {
+        $activities = $this->Activities->find('all')->where(['status_id' => 1]);
+        $today = date_create(date('m/d/Y'));
+
+        $count = 0;
+        foreach ($activities as $activity) {
+            $dateDiff = date_diff($today, date_create($activity->start_date));
+            $converted = $dateDiff->format('%R%a day(s)');
+            if ((int) $converted < 0) {
+
+                $count++;
+            }
+        }
+        return $count;
+    }
+
+    public function getIndicators()
+    {
+        $indicators = $this->Milestones->find('all')->where(['status_id' => 1]);
+        $today = date_create(date('m/d/Y'));
+
+        $count = 0;
+        $convertDate = array();
+        foreach ($indicators as $indicator) {
+            $dateDiff = date_diff($today, date_create($indicator->start_date));
+            $converted = $dateDiff->format('%R%a day(s)');
+            if ((int) $converted < 0) {
+
+                array_push($convertDate, $indicator);
+            }
+            $count++;
+        }
+        return $convertDate;
+    }
+    public function indicatorsCount()
+    {
+        $indicators = $this->Milestones->find('all')->where(['status_id' => 1]);
+        $today = date_create(date('m/d/Y'));
+
+        $count = 0;
+        foreach ($indicators as $indicator) {
+            $dateDiff = date_diff($today, date_create($indicator->start_date));
+            $converted = $dateDiff->format('%R%a day(s)');
+            if ((int) $converted < 0) {
+
+                $count++;
+            }
+        }
+        return $count;
+    }
+
 }
